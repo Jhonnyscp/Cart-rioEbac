2e6aa773e64302ec7abab8f6bc0badfae73d2a49
Jhonnyscp <123820740+Jhonnyscp@users.noreply.github.com>
2023-01-28 12:17:42 -0300

    Opção de saida do sistema
    Incluso no sistema a opção de saida para o usuário

--- Registro_Usuários_EBAC.c
+++ Registro_Usuários_EBAC.c
@@ -123,7 +123,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Resgistrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair so sistema\n\n");
 		printf("Opção:"); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -143,6 +144,11 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema.\n\n");
+			return 0;
+			break;	
 			
 			default:
 			printf("Esta opção não está disponivel!\n\n");
